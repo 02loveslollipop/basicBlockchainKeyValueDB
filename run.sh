43f001dcc2bf412ab6972658c318630a8e3ffd7f
02loveslollipop <mmonsalve2005@gmail.com>
2024-11-07 11:53:50 -0500

    feat: Add run.sh script for auto orchestrating blockchain cluster and updating host file with node IPs

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Auto orchestrate the blockchain cluster and add the ip of the nodes to the host file
+
+# Get mpi_node count from argument
+nodes=$1
+
+# Orchestrate the cluster
+docker compose up --scale node=$nodes -d # Start the cluster with the number of nodes specified in the argument
+
+# Get the ip of the nodes
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q) > ./shared/host # Get the ip of the nodes and write it to the host file
